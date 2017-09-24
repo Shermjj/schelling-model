c6cb4c181d7baaf50b4063c78cf131c7a85b2c14
selphan <me@me>
2017-09-24 20:58:29 +0800

    made a buncha changes, added server, working jupyter etc.

--- model.py
+++ model.py
@@ -7,9 +7,15 @@ import random
 class SchelModel(Model):
     def __init__(self,N,width,height,satisfaction_ratio):
         self.numagents = N
+        self.running = True
         self.grid = SingleGrid(width,height,True)
         self.schedule = RandomActivation(self)
         self.ratio = satisfaction_ratio
+        self.reached_equilibrium = False
+
+        self.meanratio = 0
+        self.meanratiosum = 0
+        self.lowestratio = 1
 
         for i in range(N):
             a = SchelAgent(i,self,random.randrange(1,3))
@@ -26,21 +32,31 @@ class SchelModel(Model):
                     y = random.randrange(self.grid.height)
 
         self.datacollector = DataCollector(
-            model_reporters={},
-            agent_reporters={"coordinates,race":lambda j:(j.pos,j.race)}
+            model_reporters={"mean ratio": lambda a: a.meanratio,
+                            "lowest ratio": lambda a: a.lowestratio},
+            agent_reporters={"coordinates":lambda j:j.pos,
+                             "race":lambda j:j.race}
         )
 
     def step(self):
         self.datacollector.collect(self)
+        self.meanratiosum = 0
+        self.meanratio = 0
+        self.lowestratio = 1
+        self.reached_equilibrium = True
         self.schedule.step()
+        if(self.reached_equilibrium == True):
+            self.running = False
+        self.meanratio = self.meanratiosum/self.numagents
+
 
 
 
 class SchelAgent(Agent):
     def __init__(self,unique_id,model,race):
         super().__init__(unique_id,model)
+        self.ratio = -1
         self.race = race
-        self.isunsatisfied = False
 
     def get_samerace(self):
         mates = self.model.grid.get_neighbors(self.pos,moore=True)
@@ -58,6 +74,15 @@ class SchelAgent(Agent):
                 diffrace += 1
         return diffrace
 
+    def get_ratio(self):
+        numadjsamerace = self.get_samerace()
+        numadjdiffrace = self.get_diffrace()
+        if(numadjsamerace + numadjdiffrace > 0):
+            ratio = numadjsamerace / (numadjsamerace + numadjdiffrace)
+            return ratio
+        else:
+            return -1
+
     def move(self):
         x = random.randrange(self.model.grid.width)
         y = random.randrange(self.model.grid.height)
@@ -71,12 +96,14 @@ class SchelAgent(Agent):
                 y = random.randrange(self.model.grid.height)
 
     def step(self):
-        numadjsamerace = self.get_samerace()
-        numadjdiffrace = self.get_diffrace()
-        if(numadjsamerace + numadjdiffrace > 0):
-            ratio = numadjsamerace / (numadjsamerace + numadjdiffrace)
-            if(ratio < self.model.ratio):
-                self.move()
+        self.ratio = self.get_ratio()
+        self.model.meanratiosum += self.ratio
+        if(self.ratio < self.model.lowestratio):
+            self.model.lowestratio = self.ratio
+        if(self.ratio < self.model.ratio):
+            self.move()
+            if(self.model.reached_equilibrium == True):
+                self.model.reached_equilibrium = False
 
 
 
---
+++ server.py
@@ -0,0 +1,38 @@
+from mesa.visualization.ModularVisualization import ModularServer
+from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.modules import ChartModule
+from mesa.visualization.UserParam import UserSettableParameter
+
+from model import SchelModel
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "Layer": 0,
+                 "r": 0.5,
+                 #"text":str(agent.unique_id),
+                 #"text_color":"yellow"
+                 }
+    if(agent.race == 1):
+        portrayal["Color"] = "red"
+    elif(agent.race == 2):
+        portrayal["Color"] = "blue"
+
+
+    return portrayal
+
+grid = CanvasGrid(agent_portrayal,50,50)
+chart1 = ChartModule([
+    {"Label":"mean ratio","Color":"Black"}],
+    data_collector_name="datacollector"
+)
+chart2 = ChartModule([
+    {"Label":"lowest ratio","Color":"Black"}],
+    data_collector_name="datacollector"
+)
+param = {"N":UserSettableParameter("slider","Number of Agents",value=500,min_value=2,max_value=2500,step=100),"width":50,"height":50,
+         "satisfaction_ratio":UserSettableParameter("slider","Satisfaction ratio",value=0.5,min_value=0,max_value=1,step=0.1)}
+
+server = ModularServer(SchelModel,[grid,chart1,chart2],"Schelling Model",param)
+
+server.port = 8521
